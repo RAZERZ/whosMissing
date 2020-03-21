85e310c92aa91e65e0d16f65553b8ee5e7f9c13d
Rami Abou Zahra <ramiabouzahra@gmail.com>
2020-03-21 13:19:42 +0100

    Base functionality in place.

--- index.js
+++ index.js
@@ -37,12 +37,39 @@ document.getElementsByClassName("schoolsoftBtn")[0].addEventListener("click", fu
             chrome.storage.sync.set(returnedNameObj);
         }
         else {
-            console.log("ERROR: User is not on the contact page.");
+            console.log("ERROR: Extension is not on the contact page.");
             chrome.storage.sync.get("name", function(r){console.log(r.name)});
         }
     });
 });
 
 document.getElementsByClassName("verifyList")[0].addEventListener("click", function() {
+
+    function returnDOM() {
+        let meetList = document.querySelectorAll('[data-sort-key]');
+        if(meetList) {
+            let arr = [];
+            for (var i = 0; meetList.length > i; i++) {
+                arr.push(meetList[i].innerText);
+            }
+            return arr;
+        }
+        else {
+            return false;
+        }
+    }
+    chrome.tabs.executeScript({
+        code: '(' + returnDOM + ')();'
+    }, function(meetListArr) {
+        chrome.storage.sync.get("name", function(response) {
+            for(var i = 0; meetListArr.length > i; i++) {
+                for(var j = 0; response.name.length > j; j++) {
+                    if(meetListArr[i][0].includes(response.name[j])) {
+                        console.log("quack");
+                    }
+                }
+            }
+        });
+    });
     //TODO: document.body.children[1].children[2].children[0].children[0].children[2].children[2].children[2].children[0].children[1].children[0].children[1].children[0].children[1].children[0].children[0].children[0].children[1].children[0].children[0] from meets.google.com
 });
