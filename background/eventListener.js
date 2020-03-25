14afe1c89214a25156289466373d7569d3e82605
Rami Abou Zahra <ramiabouzahra@gmail.com>
2020-03-25 18:10:15 +0100

    Removed duplicate code.

--- background/eventListener.js
+++ background/eventListener.js
@@ -2,19 +2,19 @@ chrome.runtime.onInstalled.addListener(function() {
 
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
         chrome.declarativeContent.onPageChanged.addRules([{
-            conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: {hostEquals: 'sms.schoolsoft.se'},
-            })
+            conditions: [
+                new chrome.declarativeContent.PageStateMatcher({
+                    pageUrl: {hostEquals: 'sms.schoolsoft.se'}
+                }),
+                new chrome.declarativeContent.PageStateMatcher({
+                    pageUrl: {hostEquals: 'meet.google.com'}
+                }),
+                new chrome.declarativeContent.PageStateMatcher({
+                    pageUrl: {hostEquals: 'razerz.github.io'}
+                })
             ],
             actions: [new chrome.declarativeContent.ShowPageAction()]
         }]);
-        chrome.declarativeContent.onPageChanged.addRules([{
-            conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: {hostEquals: 'meet.google.com'},
-            })
-            ],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-        }]);
-    });
 
+    });
 });
